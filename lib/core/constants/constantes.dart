27392dfa35bc8a63cecdb76d38c639ab655678e5
Chikage04 <104400056+Chikage04@users.noreply.github.com>
2025-02-18 00:06:23 +0100

    demineur

--- lib/core/constants/constantes.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-import 'package:flutter/material.dart';
-
-class Constantes{
-  static Color primaryColor = Color(0xFF2196F3);
-  static String appTitle = "Compteur App";
-  static String deuxiemePageTitle = "Deuxieme Page";
-}
